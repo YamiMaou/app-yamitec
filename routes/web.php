72d298638c15380a74457dfab8d3958999f639a5
Maou Yami <maou@yamitec.com>
2021-02-12 23:29:27 -0300

    send modify react files

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get( '/{path?}', function(){
     return view( 'react.index' );
 } )->where('path', '.*');
+
+Auth::routes();
 /*
 Route::resource('/', 'HomeController');
 
