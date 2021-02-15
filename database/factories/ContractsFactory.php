f482e6268a8c05fe5b63d567ee14253465c02039
Marcos Paulo <markus.rodrigues7@gmail.com>
2021-02-15 10:09:55 -0200

    models relations

--- database/factories/ContractsFactory.php
+++ database/factories/ContractsFactory.php
@@ -2,16 +2,17 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Models\Manager;
+use App\Models\Contract;
+use App\Models\Provider;
 use App\User;
 use Faker\Generator as Faker;
 
-$factory->define(Manager::class, function (Faker $faker) {
+$factory->define(Contract::class, function (Faker $faker) {
         return [
             'title' => $faker->word,
             'value' => $faker->randomDigit,
-            'user_id' => function () {
-                return factory(User::class)->create()->id;
+            'provider_id' => function () {
+                return factory(Provider::class)->create()->id;
             }
     ];
 });
