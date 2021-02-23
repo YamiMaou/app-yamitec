caa22357e2784888906236ecfa538e1fe1971c55
Marcos Paulo <markus.rodrigues7@gmail.com>
2021-02-23 10:15:37 -0300

    migrations contact and address

---
+++ api/database/migrations/2021_02_23_125520_create_contacts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('phone1', 11);
+            $table->string('phone2', 11)->nullable();
+            $table->string('email', 100);
+            $table->string('linkedin', 100);
+            $table->string('facebook', 100);
+            $table->string('instagram', 100);
+
+            $table->unsignedBigInteger('contributors_id');
+            $table->foreign('contributors_id')->references('id')->on('contributors')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
