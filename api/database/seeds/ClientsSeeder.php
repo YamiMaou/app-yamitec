8cde753816ec73fa714baec7ddd408764e21c645
Marcos Paulo <markus.rodrigues7@gmail.com>
2021-02-25 08:28:05 -0300

    ClientController and Seeder

---
+++ api/database/seeds/ClientsSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ClientsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        factory(App\Models\Client::class, 7)->create();
+    }
+}
