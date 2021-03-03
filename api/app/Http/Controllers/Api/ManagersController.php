94923f062f71fef0d1af3bef5d5d3ae0e95afad7
Marcos Paulo <markus.rodrigues7@gmail.com>
2021-03-03 17:40:22 -0300

    routes

--- api/app/Http/Controllers/Api/ManagersController.php
+++ api/app/Http/Controllers/Api/ManagersController.php
@@ -55,4 +55,61 @@ class ManagersController extends Controller
         }
 
     }
+
+    public function update(Request $resquest, $id)
+    {
+        try {
+            $client = Client::findOrFail($id);
+
+            $user = User::findOrFail($client->user_id);
+
+            $data_user = [
+                'name' => $resquest->name,
+                'email' => $resquest->email,
+                'password' => Hash::make($resquest->cpf),
+            ];
+    
+            $user->update($data_user);
+
+            $data_client = [
+                'name' => $resquest->name,
+                'cpf' => $resquest->cpf,
+                'birth_date' => $resquest->birth_date,
+                'active' => $resquest->active,
+                'note' => $resquest->note,
+                'user_id' => $user->id,
+            ];
+
+            $client->update($data_client);
+
+            $data_address = [
+                'zipcode' => $resquest->zipcode,
+                'street' => $resquest->street,
+                'city' => $resquest->city,
+                'uf' => $resquest->uf,
+                'client_id' => $client->id,
+            ];
+    
+            $address = Address::where('client_id', $client->id);
+            
+            $address->update($data_address);
+    
+            $data_contact = [
+                'phone1' => $resquest->phone1,
+                'phone2' => $resquest->phone2,
+                'email' => $resquest->email,
+                'linkedin' => $resquest->linkedin,
+                'facebook' => $resquest->facebook,
+                'instagram' => $resquest->instagram,
+                'client_id' => $client->id,
+            ];
+
+            $contact = Contact::where('client_id', $client->id);
+    
+            $contact->update($data_contact);
+
+            return response()->json(["success"=> true, "type" => "store", "message" => "Atualizado com Sucesso!"]);
+        } catch(\Exception  $error) {
+            return response()->json(["success"=> false, "type" => "error", "message" => "Problema ao Atualizar. ", "error" => $error->getMessage()], 201);
+        }
 }
